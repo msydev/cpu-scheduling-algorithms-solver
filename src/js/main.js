0b45b64c95f48854117978c9778fb5368cc5cea2
mustafa <mustafasalem2314@gmail.com>
2022-09-11 21:02:33 +0300

    fixing mobile errors

--- src/js/main.js
+++ src/js/main.js
@@ -7,31 +7,46 @@ import output from "./components/output/output.js";
 import { themeToggle } from "./components/themeToggle.js";
 import renderCpuUtilization from "./components/output/renderCpuUtilization.js";
 
-const solveBtn = document.querySelector('.solve-btn');
+const solveBtn = document.querySelector(".solve-btn");
 
 // init UI
 solveBtn.addEventListener("click", createRipple);
 handleSelectBox();
 themeToggle();
 
-solveBtn.addEventListener("click",function(e){
-    const data = getData();
-    if(data === undefined || data === null){
-        return;
-    } else{
-        const [selectedAlgo, arrivalTime, burstTime, quantumTime, priorities] = data;
-        const solved = solve(selectedAlgo ,arrivalTime, burstTime, quantumTime, priorities);
-        output(solved.ganttChartInfo, solved.solvedProcessesInfo, selectedAlgo);
-        if(window.innerWidth <= 768){           
-            setTimeout(function() {
-
-                scrollTo(
-                    0,
-                    document.getElementById('output').offsetTop,
-                    'smooth'
-                )
-            }, 100);
-        }
-        }
-    
+solveBtn.addEventListener("click", function (e) {
+  const data = getData();
+  if (data === undefined || data === null) {
+    return;
+  } else {
+    const [selectedAlgo, arrivalTime, burstTime, quantumTime, priorities] =
+      data;
+    const solved = solve(
+      selectedAlgo,
+      arrivalTime,
+      burstTime,
+      quantumTime,
+      priorities
+    );
+    output(solved.ganttChartInfo, solved.solvedProcessesInfo, selectedAlgo);
+    if (window.innerWidth <= 768) {
+      let activeEl = document.activeElement;
+      if (activeEl.classList.contains("input"))
+        hideKeyboard(document.activeElement);
+      setTimeout(function () {
+        scrollTo(0, document.getElementById("output").offsetTop, "smooth");
+      }, 100);
+    }
+  }
 });
+
+function hideKeyboard(element) {
+  element.attr("readonly", "readonly"); // Force keyboard to hide on input field.
+  element.attr("disabled", "true"); // Force keyboard to hide on textarea field.
+  setTimeout(function () {
+    element.blur(); //actually close the keyboard
+    // Remove readonly attribute after keyboard is hidden.
+    element.removeAttr("readonly");
+    element.removeAttr("disabled");
+  }, 100);
+}

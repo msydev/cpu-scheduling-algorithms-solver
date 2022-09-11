0ee88568d63bc3c0111f02f1fdde4d54384611b3
mustafa <mustafasalem2314@gmail.com>
2022-09-11 16:05:23 +0300

    add theme changing from lacal storage:

--- src/js/components/themeToggle.js
+++ src/js/components/themeToggle.js
@@ -1,9 +1,27 @@
-export function themeToggle(){
-    document.querySelector("#switch").oninput = () => {
-	if (document.querySelector("#switch").checked == true) {
-		document.querySelector("html").dataset.theme = 'dark'
-	} else {
-        document.querySelector("html").dataset.theme = 'light'
-	}
-};
+const html = document.querySelector("html");
+const siwtchToggle = document.querySelector("#switch");
+let theme = window.localStorage.getItem("data-theme");
+
+export function themeToggle() {
+  if (theme) {
+    html.dataset.theme = theme;
+    siwtchToggle.checked = theme === "dark" ? true : false;
+  } else {
+    theme = html.dataset.theme;
+    changeThemeInLocalStorage(theme);
+  }
+
+  siwtchToggle.oninput = () => {
+    if (siwtchToggle.checked == true) {
+      html.dataset.theme = "dark";
+	  changeThemeInLocalStorage('dark');
+    } else {
+      html.dataset.theme = "light";
+	  changeThemeInLocalStorage('light');
+    }
+  };
+}
+
+function changeThemeInLocalStorage(theme) {
+  window.localStorage.setItem("data-theme", theme);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -7,15 +7,14 @@ import output from "./components/output/output.js";
 import { themeToggle } from "./components/themeToggle.js";
 import renderCpuUtilization from "./components/output/renderCpuUtilization.js";
 
-// const
 const solveBtn = document.querySelector('.solve-btn');
 
 // init UI
 solveBtn.addEventListener("click", createRipple);
 handleSelectBox();
 themeToggle();
-document.getElementById
-solveBtn.addEventListener("click",function(){
+
+solveBtn.addEventListener("click",function(e){
     const data = getData();
     if(data === undefined || data === null){
         return;
@@ -24,6 +23,7 @@ solveBtn.addEventListener("click",function(){
         const solved = solve(selectedAlgo ,arrivalTime, burstTime, quantumTime, priorities);
         output(solved.ganttChartInfo, solved.solvedProcessesInfo, selectedAlgo);
         if(window.innerWidth <= 768){
+            console.log(e.currentTarget)
             scrollTo(
                 0,
                 document.getElementById('output').offsetTop,
